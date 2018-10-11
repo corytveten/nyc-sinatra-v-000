4ec74964aee192efb26839e9ab27f7ac457c099d
Cory Tveten <corytveten@gmail.com>
2018-10-11 16:24:24 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -8,4 +8,16 @@ class FiguresController < ApplicationController
   get '/figures/new' do
     erb :'figures/new'
   end
+
+  post '/figures' do
+    @figure = Figure.create(params["figure"])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
 end
